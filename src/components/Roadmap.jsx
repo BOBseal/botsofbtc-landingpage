65eb69b96bf079eb41f07bca184996ae9e9313c0
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2024-04-30 16:11:41 +0530

    headers

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -6,7 +6,7 @@ const Roadmap = () => {
   return (
     <div className='roadContainer flex p-[2.5rem] flex-col justify-center gap-[1rem] md:gap-[2rem]'>
       <div className='flex gap-[0.5rem]'>
-        <h2 className='text-[24px] pb-[1rem] md:text-[60px] font-[700] md:ml-[3rem]'>ROADMAP</h2>
+        <h2 className='text-[24px] pb-[1rem] md:text-[60px] font-[700] md:ml-[3rem] font-nunito'>ROADMAP</h2>
       </div>
       <div className='flex flex-col gap-[1rem] md:gap-[2rem] md:ml-[4rem]'>
           <p>We do not have a fixed timeline for roadmap products as we believe a fixed List of Shipments , over fixed Points of Time do not adequtely reflects the needs for an Ever-Evolving and Dynamic Crypto Market. Thus we have only pointed out the Initial Products Planned in Chronological Order and is Open to Community-Voted changes accomodating to the requirements of IRL Market Conditions.</p>
--- src/components/Utilities.jsx
+++ src/components/Utilities.jsx
@@ -5,9 +5,9 @@ import "../styles/Utilities.css"
 export const Utilities = () => {
   return (
     <div className='util-container'>
-        <h2>NFT UTILITIES AND FEATURES:</h2>
+        <h2 className='font-nunito'>NFT UTILITIES AND FEATURES:</h2>
         <p>The BOTS OF BITCOIN PFP Collection is the Exclusive NFT Collection for BOTS OF BITCOIN Ecosystem and Enjoys a Range of Benifits for Both Holders and OG Minters.</p>
-        <h3>Holder Utility & Benifits :</h3>
+        <h3 className='font-nunito text-[1.25rem]'>Holder Utility & Benifits :</h3>
         <li>Revenue Distribution : Each NFT is Eligible to Earn 30% of all Platform Revenue via Staking.</li>
         <li>Governance: Along with Our Governance Token, Each NFT will also serve as a means allow its holder to participate in Governance of the Platform.</li>
         <li>Discounts & Bonuses: Get Fee Discounts and Bonuses for Most Platform Activity and Services.</li>
@@ -15,7 +15,7 @@ export const Utilities = () => {
         <li>Airdrop : A Total of 2% Supply is Guaranteed as Airdrop for Holders and an Additional 3% are allocated as Bonuses to be won by BOTS Role Holders through Various Events.</li>
         <li>BOTS ROLE : PFP Holders get a Special Role called "BOTS". </li>
 
-        <h3>Genesis Minter Benifits: </h3>
+        <h3 className='text-[1.25rem] font-nunito'>Genesis Minter Benifits: </h3>
         <li>6.9% Royalties Forever : All addresses which participate in the Genesis Mint and mints a PFP are eligible to Earn 6.9% Royalty from secondary sales of their minted PFPs Forever!</li>
         <li>BOTS ROLE  : Genesis minting participants will be counted as a "BOTS" regardless if they Hold their PFP and will get to keep their role.</li>
     </div>
