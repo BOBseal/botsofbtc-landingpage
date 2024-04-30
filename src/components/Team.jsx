7fc1977fd7015dc3adbc0dce7d87de9ed1a611ea
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2024-04-30 19:28:54 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -9,7 +9,7 @@ const Team = () => {
       <div className=' w-full h-full justify-between flex flex-col items-center gap-[2rem]'>
         <h1 className='text-white text-[2rem] md:text-[3rem] font-nunito w-[80%] md:w-auto md:justify-center justify-start'>TEAM</h1>
         <div className='w-full h-full min-h-[19rem] grid-cols-2 md:grid-cols-4 grid gap-[2rem]'>
-        {teamConfig.map((item)=>(
+        {teamConfig.map((item, key)=>(
           <TeamCard item ={item}/>
         ))}
         </div>
