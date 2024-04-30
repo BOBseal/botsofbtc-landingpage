f8827396c23b4d8aad11cb3ef5d7b5a87483d7bd
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2024-04-30 19:23:20 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,10 +6,13 @@ import TeamCard from "../components/CARDS/TeamCard"
 const Team = () => {
   return (
     <div className='teamContainer h-full p-[2rem] min-h-[20rem] flex justify-center items-center'>
-      <div className=' w-full h-full min-h-[19rem] grid-cols-2 md:grid-cols-4 grid gap-[2rem]'>
+      <div className=' w-full h-full justify-between flex flex-col items-center gap-[3rem]'>
+        <h1 className='text-white text-[2rem] font-nunito'>TEAM</h1>
+        <div className='w-full h-full min-h-[19rem] grid-cols-2 md:grid-cols-4 grid gap-[2rem]'>
         {teamConfig.map((item)=>(
           <TeamCard item ={item}/>
         ))}
+        </div>
       </div>
     </div>
   )
