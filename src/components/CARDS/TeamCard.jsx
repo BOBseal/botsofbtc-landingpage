ebbc4a6f938d1708e61230d9e7bfcf787a04d589
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2024-05-02 10:08:16 +0530

    f

--- src/components/CARDS/TeamCard.jsx
+++ src/components/CARDS/TeamCard.jsx
@@ -8,8 +8,8 @@ const TeamCard = ({item}) => {
   return (
     <div className='flex p-[1rem] flex-col gap-[1rem] justify-center items-center font-nunito text-black'>
         <Image src={item.picture} height={100} width={100} alt ={item.name} className="rounded-full border-black border"/>
-        <p>@{item.name}</p>
-        <p className='text-[15px] md:text-[20px]'>{item.designation}</p>
+        <p className='text-[15px] md:text-[20px] font-semibold'>@{item.name}</p>
+        <p className='text-[15px] md:text-[20px] font-semibold'>{item.designation}</p>
         <div className='flex gap-4'>
             {link ? <Image src={twit} height={30} width={30} className="bg-black p-1"/>:<div className='w-full flex justify-center'>
             <p className='text-[10px] md:text-[14px] w-[80%] h-[3rem] md:min-h-[6rem] lg:w-[65%]'>{item.desc}</p>
--- src/configs/config.jsx
+++ src/configs/config.jsx
@@ -7,7 +7,7 @@ export const teamConfig =[
     {id:1 , name : "BOB" , designation:"Founder & CTO", desc:"Web3 Dev with a Vision to Serve the Ecosystem and Grow Along.", link:"", picture: bob},
     {id:2 , name : "S. Agarwal" , designation:"Legal Head", desc:"CA with magic-hand in finances & handles compliances.", link:"", picture: six},
     {id:3 , name : "Deep" , designation:"Graphics Lead", desc:"Funny & Obsessed Artist with an eye for details and a punch in his Artworks.", link:"", picture: deep},
-    {id:4 , name : "V" , designation:"COO / Advisory", desc:"Ex-Yahoo R&D , Co-Operate with founder & team to plan the bigger picture.", link:"", picture: v},
+    {id:4 , name : "Vishnu" , designation:"COO / Advisory", desc:"Ex-Yahoo R&D , Co-Operate with founder & team to plan the bigger picture.", link:"", picture: v},
 ]
 
 export const partners =[
