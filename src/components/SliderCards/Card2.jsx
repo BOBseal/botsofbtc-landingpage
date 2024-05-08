5f30ea56c3efb372989ca001547551cf0163ee6e
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2024-05-08 22:34:09 +0530

    Update Card2.jsx

--- src/components/SliderCards/Card2.jsx
+++ src/components/SliderCards/Card2.jsx
@@ -8,7 +8,7 @@ const Card2 = () => {
         <p className='w-[90%] text-white font-nunito text-[15px]'>{sectionText[1].p1}</p>
         <h2 className='text-[#E5BD19] font-fredoka text-[18px] md:text-[35px] font-bold uppercase md:w-[90%]'>{sectionText[1].p2[0].h}</h2>
         {sectionText[1].p2[0].t.map((obj , key)=>(
-            <li className='w-[90%] text-white font-nunito text-[15px]'>{obj}</li>
+            <li key={key} className='w-[90%] text-white font-nunito text-[15px]'>{obj}</li>
         ))}
         <h2 className='text-[#E5BD19] font-fredoka text-[18px] md:text-[35px] font-bold uppercase md:w-[90%]'>{sectionText[1].p2[1].h}</h2>
         {sectionText[1].p2[1].t.map((obj , key)=>(
