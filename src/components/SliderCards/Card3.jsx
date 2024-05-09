1b6e7bc2c09a520ee29fd4c7df4e60628fcdee28
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2024-05-09 16:04:01 +0530

    just

--- src/components/SliderCards/Card3.jsx
+++ src/components/SliderCards/Card3.jsx
@@ -4,7 +4,7 @@ import {sectionText} from "../../configs/config"
 
 const Card3 = () => {
   return (
-    <div className='w-full h-full text-white gap-[1rem] flex flex-col'>
+    <div className='w-full h-full justify-evenly text-white gap-[1rem] flex flex-col'>
         <h1 className='text-[#E5BD19] font-fredoka text-[22px] leading-[30px] md:leading-[60px] md:text-[55px] font-bold uppercase md:w-[90%] flex justify-start'>{sectionText[2].h1}</h1>
         <p className='w-[90%] text-white text-[15px] font-nunito'>{sectionText[2].p1}</p>
         <h2 className='text-[#E5BD19] leading-[30px] md:leading-[50px] font-fredoka text-[18px] md:text-[35px] font-bold uppercase md:w-[90%]'>{sectionText[2].h2}</h2>
