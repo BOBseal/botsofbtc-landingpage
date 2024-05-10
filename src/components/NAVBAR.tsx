63e444b49b6112d7a6a1c319057c4ca7db8adc57
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2024-05-10 21:31:52 +0530

    Update NAVBAR.tsx

--- src/components/NAVBAR.tsx
+++ src/components/NAVBAR.tsx
@@ -36,11 +36,7 @@ const Navbar: React.FC<NavbarProps> = () => {
             <motion.div
             initial={{ opacity: 1}}
             animate={{ opacity: 1 }}
-            transition={{
-              ease: "linear",
-              duration: 2,
-              y: { duration: 1 }
-            }}
+            transition={{ type: "spring", stiffness: 100 }}
             exit={{ opacity: 0}}
             >
              <MobileNav openMobileMenu={openMobileMenu} userWallet={user.wallet} connectWallet={connectWallet}/>
