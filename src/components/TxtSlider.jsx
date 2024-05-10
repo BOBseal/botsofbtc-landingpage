05f366319829a8b9fb3771d8c3f882b642db3c06
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2024-05-10 15:33:16 +0530

    slider animation

--- src/components/TxtSlider.jsx
+++ src/components/TxtSlider.jsx
@@ -11,6 +11,7 @@ import Card4 from "./SliderCards/Card4"
 import Card5 from "./SliderCards/Card5"
 import Card6 from "./SliderCards/Card6"
 import Card7 from './SliderCards/Card7'
+import { motion, AnimatePresence } from "framer-motion"
 
 const TxtSlider = () => {
     const [act , setAct] = useState(1)
@@ -38,41 +39,33 @@ const TxtSlider = () => {
         setAct(s -1);
     }
 
+    const exitVariants = {
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+    };
+
   return (
     <div className='bg-[#231F20] flex justify-between flex-col md:flex-row md:justify-between items-center h-[600px] md:h-[790px] overflow-x-auto pt-[2rem] pb-[2rem] border-b border-white'>
         <button className='text-white md:ml-[30px] hidden md:flex' onClick={()=> goLeft()}><Image src={l} height={20} width={20} className="hover:scale-125"/></button>
         
-        <div className={`h-[86%] flex flex-col md:w-[70%] lg:w-[55%] w-[85%] ${act == 1 ? "" : "hidden"}`}>
-            <Card1/>
-            
-        </div>
+        <AnimatePresence mode="wait">
+                <motion.div
+                    key={act}
+                    className={`h-[86%] flex flex-col md:w-[70%] lg:w-[55%] w-[85%]`}
+                    initial="hidden"
+                    animate="visible"
+                    exit="hidden"
+                    variants={exitVariants}
+                >
+                    {act === 1 && <Card1 />}
+                    {act === 2 && <Card2 />}
+                    {act === 3 && <Card3 />}
+                    {act === 4 && <Card4 />}
+                    {act === 5 && <Card5 />}
+                    {act === 6 && <Card6 />}
+                </motion.div>
+            </AnimatePresence>
 
-        <div className={`h-[86%] flex flex-col md:w-[70%] lg:w-[55%] w-[85%] ${act == 2 ? "" : "hidden"}`}>
-            <Card2/>
-        </div>
-
-
-        <div className={`h-[86%] flex flex-col md:w-[70%] lg:w-[55%] w-[85%] ${act == 3 ? "" : "hidden"}`}>
-            <Card3/>
-        </div>
-
-
-        <div className={`h-[86%] flex flex-col md:w-[70%] lg:w-[55%] w-[85%] ${act == 4 ? "" : "hidden"}`}>
-            <Card4/>
-        </div>
-
-
-
-        <div className={`h-[86%] flex flex-col md:w-[70%] lg:w-[55%] w-[85%] ${act == 5 ? "" : "hidden"}`}>
-            <Card5/>
-        </div>
-
-
-
-        <div className={`h-[86%] flex flex-col md:w-[70%] lg:w-[55%] w-[85%] ${act == 6 ? "" : "hidden"}`}>
-            <Card6/>
-            
-        </div>
         <div className='flex w-full justify-between mt-[300px] md:hidden h-full absolute'>
             <button className='text-white md:hidden flex' onClick={()=> goLeft()}><Image src={l} height={20} width={20} className="hover:scale-125"/></button>
             <button className='text-white md:hidden flex' onClick={()=> goRight()}><Image src={r} height={20} width={20} className="hover:scale-125"/></button>    
