0f28662533d33137136eaaf3be6a0406dc6a5726
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2024-05-10 22:24:03 +0530

    Update Partners.jsx

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -9,7 +9,7 @@ const Partners = () => {
 
   return (
     <div className={`bg-[#231F20] flex p-[2rem] md:pb-[4rem] md:pt-[3rem] gap-[2rem] flex-col items-center ${states.mobileMenuOpen ? "blur-lg" : ""}`}>
-      <p className='text-white font-nunito underline underline-offset-3'>Launch Partners</p>
+      <p className='text-white font-nunito underline underline-offset-3'>Powered by</p>
       <div className='w-full flex gap-[1rem] justify-center'>
         {partnerInfo.map((o,k)=>(
           <Image src={o.logo} key={k} width={180} height={180} alt="BOB Logo" className='w-[100px] md:w-[180px]'/>
