e0da49b182e28dea334dbedf739815ee1f1a6a0e
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2024-05-30 06:38:37 +0530

    Update MintCard.jsx

--- src/components/CARDS/MintCard.jsx
+++ src/components/CARDS/MintCard.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Image from '../../../node_modules/next/image'
-import ff from "../../assets/sobdemo.png"
+import ff from "../../assets/skibidi.gif"
 import escan from "../../assets/etherscan.png"
 import ele from "../../assets/element.png"
 
