121a8660c9f7d1af7f11bb0a29d8b1c1de4da9e0
bobseal <bhuyanbishal999@gmail.com>
2025-01-04 16:36:10 +0530

    move to context

--- src/components/CARDS/ClaimSpice.jsx
+++ src/components/CARDS/ClaimSpice.jsx
@@ -4,86 +4,8 @@ import { AppContext } from "@/context/AppContext";
 import { ethers } from "ethers";
 import { getBobNftCa, getNFTCa, getRpCoreContract } from "@/utils/hooks";
 const ClaimSpice = () => {
-    const { connectWallet, user } = useContext(AppContext)
-    const [data, setData] = useState({
-        spicePerBOB: 500,
-        spicePerSOB: 100,
-        spicePerRP: 0.075,
-        maxRpCalculated: 150000
-    })
-    const [loaders, setLoaders] = useState({
-        initial: false,
-        claim: "Claim Spice",
-        bonus: "Claim Bonus",
-        vote:"Voter Claim"
-    })
+    const { connectWallet, user, getSpiceData, loaders, setLoaders, data , setData } = useContext(AppContext)
     const apiUrl = `https://api.botsofbtc.com`
-
-    const getSpiceData = async () => {
-        try {
-            setLoaders({ ...loaders, initial: true })
-            let unclaimedIds = []
-            let claimableMintSpice = 0;
-            const userFormat = ethers.utils.getAddress(user.wallet)
-            const bobCa = await getBobNftCa(user.wallet)
-            const sobCa = await getNFTCa(user.wallet)
-            const coreCa = await getRpCoreContract(user.wallet);
-            const bobHolderList = await fetch(`${apiUrl}/holderListBOB`);
-            const eligibleSpice = await fetch(`${apiUrl}/eligibleSobSpice?address=${user.wallet}`)
-            let vtSpc = 0;
-            let elig = false;
-            if(eligibleSpice.status == 200){
-                const response = await eligibleSpice.json();
-                elig = true;
-                vtSpc = response.claimable 
-            } 
-            if(eligibleSpice.status == 400){
-                vtSpc = "Ineligible or Already Claimed"
-            }
-            if (bobHolderList.ok) {
-
-                const holderListData = await bobHolderList.json();
-                const holderData = holderListData[userFormat];
-                //console.log(dailyClaimSpice.claimable)
-                if (holderData) {
-                    for (const j of holderData.ownedIds) {
-                        const ic = await fetch(`${apiUrl}/idClaimed?tokenId=${j}`)
-                        const isClaimed = await ic.json();
-                        if (!isClaimed) {
-                            claimableMintSpice = claimableMintSpice + 1000;
-                            //claimableIds = claimableIds + 1;
-                            unclaimedIds.push(j);
-                        }
-                    }
-                }
-            }
-            //console.log(vtSpc)
-            const bobHeld = await bobCa.balanceOf(user.wallet)
-            const sobHeld = await sobCa.balanceOf(user.wallet)
-            const rpData = await coreCa.getUser(user.wallet)
-            const dca = await fetch(`${apiUrl}/claimableSpice?address=${userFormat}`)
-            const dCa = await dca.json();
-            const dailyClaim = dCa.claimable;
-            setData({
-                ...data,
-                bobHeld: Number(bobHeld),
-                sobHeld: Number(sobHeld),
-                unclaimedIds: unclaimedIds,
-                claimableIds: unclaimedIds.length,
-                rpBalance: Number(rpData[1]),
-                dailyClaimable: dailyClaim,
-                mintClaimable: claimableMintSpice,
-                totalClaimable: Number(claimableMintSpice) + Number(dailyClaim),
-                voteSpice:vtSpc,
-                eligible:elig,
-                processed: true
-            })
-            setLoaders({ ...loaders, initial: false })
-        } catch (error) {
-            console.log(error)
-            setLoaders({ ...loaders, initial: false })
-        }
-    }
 
     const claimDailySpice = async () => {
         try {
@@ -161,11 +83,17 @@ const ClaimSpice = () => {
             console.log(error)
         }
     }
-
+    const handler =async()=>{
+        try {
+            await getSpiceData();
+        } catch (error) {
+            console.log(error);
+        }
+    }
     useEffect(() => {
         if (user.wallet) {
             const timeout = setTimeout(() => {
-                getSpiceData();
+                handler();
             }, 300); // Debounce delay
     
             return () => clearTimeout(timeout); // Cleanup
